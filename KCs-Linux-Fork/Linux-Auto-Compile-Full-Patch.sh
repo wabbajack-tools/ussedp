288c905d5662c92b8bc168c285af0d82093296d3
KCkingcollin <34804902+KCkingcollin@users.noreply.github.com>
2021-11-26 02:29:12 -0500

    Create Linux-Auto-Compile-Full-Patch.sh
    supper simple script to compile and run the ussedp app in linux

---
+++ KCs-Linux-Fork/Linux-Auto-Compile-Full-Patch.sh
@@ -0,0 +1,30 @@
+#/bin/bash
+
+cd ~/
+
+git clone https://github.com/wabbajack-tools/ussedp
+
+cd ussedp
+
+read -s -p "Enter Password for sudo: " sudoPW
+echo $sudoPW | sudo -S dotnet build
+
+
+if [ -d "Patch Files-57618-1-5-1-1637899588.7z?token=KhZ-MZViNgaUYlpcobtoSQ&expires=1637920582&user_id=36400125&rip=75.130.137.33" ]
+then
+    wget "https://premium-b.nexus-cdn.com/1704/57618/Patch Files-57618-1-5-1-1637899588.7z?token=KhZ-MZViNgaUYlpcobtoSQ&expires=1637920582&user_id=36400125&rip=75.130.137.33"
+fi
+
+7z x -y 'Patch Files-57618-1-5-1-1637899588.7z?token=KhZ-MZViNgaUYlpcobtoSQ&expires=1637920582&user_id=36400125&rip=75.130.137.33'
+
+rsync -avx ~/ussedp/FullPatcher/ ~/ussedp/Patcher/bin/Debug/net6.0/
+
+mkdir Linux-ussedp-FullPatcher
+
+rsync -avx ~/ussedp/Patcher/bin/Debug/net6.0/ ~/Linux-ussedp-FullPatcher/
+
+ln -s ~/Linux-ussedp-FullPatcher/Patcher ~/ussedp-FullPatcher
+
+cd ~/
+
+./ussedp-FullPatcher
