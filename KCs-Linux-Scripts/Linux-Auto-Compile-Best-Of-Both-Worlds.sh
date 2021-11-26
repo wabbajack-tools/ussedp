ffe5f1d151a5821ebef82d40ab8b3d71e6a177ef
CrezyDud <84625713+CrezyDud@users.noreply.github.com>
2021-11-26 11:29:41 +0100

    CLEEEEEN YES

--- KCs-Linux-Scripts/Linux-Auto-Compile-Best-Of-Both-Worlds.sh
+++ KCs-Linux-Scripts/Linux-Auto-Compile-Best-Of-Both-Worlds.sh
@@ -1,13 +1,14 @@
 #/bin/bash
 
-cd /home/$(whoami)/
+cd ~
 
 git clone https://github.com/wabbajack-tools/ussedp
 
 cd ussedp
 
-read -s -p "Enter Password for sudo: " sudoPW
-echo $sudoPW | sudo -S dotnet build
+echo "Enter You Password Below:"
+
+sudo dotnet build
 
 
 if [ ! -f "Patch Files-57618-1-5-1-1637899588.7z?token=KhZ-MZViNgaUYlpcobtoSQ&expires=1637920582&user_id=36400125&rip=75.130.137.33" ]
@@ -17,33 +18,33 @@ else
     echo "patch files already downloaded"
 fi
 
-if [ ! -d "/home/$(whoami)/ussedp/BestOfBothWorlds" ]
+if [ ! -d "~ussedp/BestOfBothWorlds" ]
 then
     7z x -y 'Patch Files-57618-1-5-1-1637899588.7z?token=KhZ-MZViNgaUYlpcobtoSQ&expires=1637920582&user_id=36400125&rip=75.130.137.33'
 else
     echo "patch files already installed"
 fi
 
-echo $sudoPW | sudo -S rsync -avx /home/$(whoami)/ussedp/BestOfBothWorlds/ /home/$(whoami)/ussedp/Patcher/bin/Debug/net6.0/
+rsync -avx ~ussedp/BestOfBothWorlds/ ~ussedp/Patcher/bin/Debug/net6.0/
 
-if [ -d "/home/$(whoami)/Linux-ussedp-BestOfBothWorlds" ]
+if [ -d "~Linux-ussedp-BestOfBothWorlds" ]
 then
-    echo $sudoPW | sudo -S rm -r /home/$(whoami)/Linux-ussedp-BestOfBothWorlds/
+    rm -r ~Linux-ussedp-BestOfBothWorlds/
 else
     echo "dir ready"
 fi
 
-echo $sudoPW | sudo -S rsync -avx /home/$(whoami)/ussedp/Patcher/bin/Debug/net6.0/ /home/$(whoami)/Linux-ussedp-BestOfBothWorlds/
+rsync -avx ~ussedp/Patcher/bin/Debug/net6.0/ ~Linux-ussedp-BestOfBothWorlds/
 
-if [ -f "/home/$(whoami)/ussedp-BestOfBothWorlds.sh" ]
+if [ -f "~ussedp-BestOfBothWorlds.sh" ]
 then
-    echo $sudoPW | sudo -S rm /home/$(whoami)/ussedp-BestOfBothWorlds.sh
+    -S rm ~ussedp-BestOfBothWorlds.sh
 else
     echo "script ready"
 fi
 
-printf "#/bin/bash \ncd /home/$(whoami)/Linux-ussedp-BestOfBothWorlds/\n./Patcher\n" > /home/$(whoami)/ussedp-BestOfBothWorlds.sh
+printf "#/bin/bash \ncd ~Linux-ussedp-BestOfBothWorlds/\n./Patcher\n" > ~ussedp-BestOfBothWorlds.sh
 
-echo $sudoPW | sudo -S chmod +x /home/$(whoami)/ussedp-BestOfBothWorlds.sh
+chmod +x ~ussedp-BestOfBothWorlds.sh
 
-/home/$(whoami)/ussedp-BestOfBothWorlds.sh
+~ussedp-BestOfBothWorlds.sh
