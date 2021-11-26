334098c21ddefe77cd93ccebc810c3b66eee01f6
KCkingcollin <34804902+KCkingcollin@users.noreply.github.com>
2021-11-26 15:40:51 -0500

    Update Linux-Auto-Compile-Full-Patch.sh
    not tested yet but should work the same as the other one

--- KCs-Linux-Scripts/Linux-Auto-Compile-Full-Patch.sh
+++ KCs-Linux-Scripts/Linux-Auto-Compile-Full-Patch.sh
@@ -6,10 +6,6 @@ git clone https://github.com/wabbajack-tools/ussedp
 
 cd ussedp
 
-echo "Enter Your sudo Password Below:"
-
-sudo dotnet build -c release
-
 if [ ! -f "ussedp-patches.7z" ]
 then
     wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1lDxHD_bDwltsp88fhazXTngVozr6TDXh' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1lDxHD_bDwltsp88fhazXTngVozr6TDXh" -O ussedp-patches.7z && rm -rf /tmp/cookies.txt
@@ -24,7 +20,11 @@ else
     echo "patch files already installed"
 fi
 
-rsync -avx ~/ussedp/FullPatcher/ ~/ussedp/Patcher/bin/Release/net6.0/
+echo "Enter Your sudo Password Below:"
+
+sudo dotnet build -c release
+
+sudo rsync -avx ~/ussedp/FullPatcher/ ~/ussedp/Patcher/bin/Release/net6.0/
 
 if [ -d "/home/$(whoami)/Linux-ussedp-FullPatcher" ]
 then
