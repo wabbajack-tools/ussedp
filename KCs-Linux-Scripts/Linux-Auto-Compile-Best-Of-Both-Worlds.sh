0c9e1c19e2e7408622fcd0dcd9f20e639a35435f
CrezyDud <84625713+CrezyDud@users.noreply.github.com>
2021-11-26 11:31:40 +0100

    CLEEEEEN(R) YEEEYYS

--- KCs-Linux-Scripts/Linux-Auto-Compile-Best-Of-Both-Worlds.sh
+++ KCs-Linux-Scripts/Linux-Auto-Compile-Best-Of-Both-Worlds.sh
@@ -18,33 +18,33 @@ else
     echo "patch files already downloaded"
 fi
 
-if [ ! -d "~ussedp/BestOfBothWorlds" ]
+if [ ! -d "~/ussedp/BestOfBothWorlds" ]
 then
     7z x -y 'Patch Files-57618-1-5-1-1637899588.7z?token=KhZ-MZViNgaUYlpcobtoSQ&expires=1637920582&user_id=36400125&rip=75.130.137.33'
 else
     echo "patch files already installed"
 fi
 
-rsync -avx ~ussedp/BestOfBothWorlds/ ~ussedp/Patcher/bin/Debug/net6.0/
+rsync -avx ~/ussedp/BestOfBothWorlds/ ~/ussedp/Patcher/bin/Debug/net6.0/
 
-if [ -d "~Linux-ussedp-BestOfBothWorlds" ]
+if [ -d "~/Linux-ussedp-BestOfBothWorlds" ]
 then
-    rm -r ~Linux-ussedp-BestOfBothWorlds/
+    rm -r ~/Linux-ussedp-BestOfBothWorlds/
 else
     echo "dir ready"
 fi
 
-rsync -avx ~ussedp/Patcher/bin/Debug/net6.0/ ~Linux-ussedp-BestOfBothWorlds/
+rsync -avx ~/ussedp/Patcher/bin/Debug/net6.0/ ~/Linux-ussedp-BestOfBothWorlds/
 
-if [ -f "~ussedp-BestOfBothWorlds.sh" ]
+if [ -f "~/ussedp-BestOfBothWorlds.sh" ]
 then
-    -S rm ~ussedp-BestOfBothWorlds.sh
+    -S rm ~/ussedp-BestOfBothWorlds.sh
 else
     echo "script ready"
 fi
 
-printf "#/bin/bash \ncd ~Linux-ussedp-BestOfBothWorlds/\n./Patcher\n" > ~ussedp-BestOfBothWorlds.sh
+printf "#/bin/bash \ncd ~/Linux-ussedp-BestOfBothWorlds/\n./Patcher\n" > ~/ussedp-BestOfBothWorlds.sh
 
-chmod +x ~ussedp-BestOfBothWorlds.sh
+chmod +x ~/ussedp-BestOfBothWorlds.sh
 
-~ussedp-BestOfBothWorlds.sh
+~/ussedp-BestOfBothWorlds.sh
