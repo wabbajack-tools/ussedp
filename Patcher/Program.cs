edc4e3b0ae4dad5434b2162a9c5e8bb8839b743e
Timothy Baldridge <tbaldridge@gmail.com>
2022-01-10 18:12:27 -0700

    Fix build errors

--- Patcher/Program.cs
+++ Patcher/Program.cs
@@ -1,13 +1,6 @@
 ﻿using System;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.ReactiveUI;
-using DTOs;
-using Wabbajack.Paths;
-using Wabbajack.Paths.IO;
 
 namespace Patcher
 {
